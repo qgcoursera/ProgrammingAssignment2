2278d6ba9c2f8e75456c77509851ea1641e53b3f
qgcoursera <qg.msapp@gmail.com>
2016-01-22 17:54:17 -0800

    Completed code for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## c <- makeCacheMatrix(x) creates a 'cacheable' object 'c' of a given matrix 'x',
+## to be passed as a parameter to cacheResolve(m)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Returned cache object of matrix x
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## i <- cacheSolve(c) returns the solve() result 'i' of a cacheable matrix object 'c' 
+## (see makeCacheMatrix to create 'c')
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached solve of matrix data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
